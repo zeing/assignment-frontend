3c1ad7656b92380fa6aabf352fdad4fa58dd12e9
WWZ <zeing@outlook.com>
2019-07-05 23:02:23 +0700

    feat: add Main layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <TopBar/>
+    <slot></slot>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import TopBar from '@/components/TopBar.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+  name: 'mainLayout',
+  components: {
+    TopBar,
+    Footer
+  }
+}
+</script>
