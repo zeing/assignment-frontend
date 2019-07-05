3b378b76800bec6657c6e7e0bbc2c2cc01f04f9c
WWZ <zeing@outlook.com>
2019-07-06 02:30:56 +0700

    feat: add scg page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import SCG from './views/SCG.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/scg',
+      name: 'scg',
+      component: SCG
     },
     {
       path: '/about',
---
+++ src/views/SCG.vue
@@ -0,0 +1,57 @@
+<template>
+  <b-container fluid >
+    <b-row  class="justify-content-md-center">
+<!--        <b-card v-for="(data, index) in restaurant" :key="index"-->
+<!--                :title="data.name"-->
+<!--                img-top-->
+<!--                tag="article"-->
+<!--                style="max-width: 20rem;"-->
+<!--                class="mb-2 mr-4"-->
+<!--        >-->
+<!--          <b-card-text>-->
+<!--            {{data.formatted_address}}-->
+<!--          </b-card-text>-->
+
+<!--&lt;!&ndash;          <b-button href="#" variant="primary">Go somewhere</b-button>&ndash;&gt;-->
+<!--        </b-card>-->
+        <b-card :header="data.name" class="text-center mb-2 mr-4" v-for="(data, index) in restaurant" :key="index"
+                border-variant="primary"
+                header-bg-variant="primary"
+                header-text-variant="white"
+                align="center"
+                style="max-width: 20rem;">
+          <b-card-text>{{data.formatted_address}}</b-card-text>
+        </b-card>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import serviceRestaurant from '@/services/restaurant'
+
+export default {
+  name: 'SCG',
+  props: {
+    msg: String
+  },
+  data: () => ({
+    restaurant: null
+  }),
+  mounted () {
+    serviceRestaurant().then(data => {
+      this.restaurant = data
+      console.log(data)
+    })
+  },
+  methods: {
+    getRestaurantImage () {
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+
+</style>
