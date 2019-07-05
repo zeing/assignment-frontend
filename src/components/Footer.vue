bbdd02753f76d06df6d181409497063c552b0fc1
WWZ <zeing@outlook.com>
2019-07-05 23:01:58 +0700

    feat: add Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info" fixed="bottom">
+    <b-navbar-brand href="#">&copy; 2019 Copyright: Witthawin Sirisiwaphak</b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+
+</style>
