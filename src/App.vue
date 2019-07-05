a4b3f351a5e6d1e07dcd23a508ca99442c333e02
WWZ <zeing@outlook.com>
2019-07-05 23:02:43 +0700

    chore: install bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <MainLayout>
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
-    </div>
+    </MainLayout>
     <router-view/>
   </div>
 </template>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-
-#nav
-  padding 30px
-  a
-    font-weight bold
+  #app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
     color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+
+  #nav
+    padding 30px
+
+    a
+      font-weight bold
+      color #2c3e50
+
+      &.router-link-exact-active
+        color #42b983
 </style>
+
+<script>
+import MainLayout from './layouts/MainLayout.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MainLayout
+  }
+}
+
+</script>
