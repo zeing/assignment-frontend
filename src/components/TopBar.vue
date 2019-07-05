274ab6ad8b2322b403a092c02402052f8bf55235
WWZ <zeing@outlook.com>
2019-07-05 23:02:06 +0700

    feat: add Topbar component

---
+++ src/components/TopBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Frontend</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'TopBar'
+}
+</script>
+
+<style scoped>
+
+</style>
