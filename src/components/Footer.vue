aef04f3e187bfb601ff1a1eb764e4e1e2562d05a
WWZ <zeing@outlook.com>
2019-07-07 02:15:36 +0700

    chore: remove link from footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <b-navbar toggleable="lg" type="dark" variant="primary" fixed="bottom">
-    <b-navbar-brand href="#">&copy; 2019 Copyright: Witthawin Sirisiwaphak</b-navbar-brand>
+    <b-navbar-brand >&copy; 2019 Copyright: Witthawin Sirisiwaphak</b-navbar-brand>
   </b-navbar>
 </template>
 
