391c6b5cafbaba58398fdf636761064b42658bf1
WWZ <zeing@outlook.com>
2019-07-06 11:01:20 +0700

    feat: adjust style scg page

--- src/views/SCG.vue
+++ src/views/SCG.vue
@@ -1,19 +1,6 @@
 <template>
   <b-container fluid >
     <b-row  class="justify-content-md-center">
-<!--        <b-card v-for="(data, index) in restaurant" :key="index"-->
-<!--                :title="data.name"-->
-<!--                img-top-->
-<!--                tag="article"-->
-<!--                style="max-width: 20rem;"-->
-<!--                class="mb-2 mr-4"-->
-<!--        >-->
-<!--          <b-card-text>-->
-<!--            {{data.formatted_address}}-->
-<!--          </b-card-text>-->
-
-<!--&lt;!&ndash;          <b-button href="#" variant="primary">Go somewhere</b-button>&ndash;&gt;-->
-<!--        </b-card>-->
         <b-card :header="data.name" class="text-center mb-2 mr-4" v-for="(data, index) in restaurant" :key="index"
                 border-variant="primary"
                 header-bg-variant="primary"
