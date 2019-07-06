226e800fcdb84b6be4efe76aa37c23a22a2b919c
WWZ <zeing@outlook.com>
2019-07-06 11:12:58 +0700

    chore: hide menu

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -4,7 +4,7 @@
     <router-link tag="b-navbar-brand" to="/">Frontend</router-link>
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-navbar-nav>
-      <router-link tag="b-nav-item" :to="{ name: 'about'}">Resume</router-link>
+<!--      <router-link tag="b-nav-item" :to="{ name: 'about'}">Resume</router-link>-->
       <router-link tag="b-nav-item" :to="{ name: 'scg'}">SCG</router-link>
     </b-navbar-nav>
   </b-navbar>
